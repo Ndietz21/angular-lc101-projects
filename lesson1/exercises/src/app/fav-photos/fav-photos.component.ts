7aa8f9d9a37d3df220ef91a4f2746abfcc70c9ec
Nolan <ndietz2121@gmail.com>
2022-12-12 15:16:44 -0600

    Lesson 1 Exercises

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Random Images';
+  image1 = 'https://images.pexels.com/photos/618833/pexels-photo-618833.jpeg?cs=srgb&dl=pexels-sagui-andrea-618833.jpg&fm=jpg';
+  image2 = 'https://fanatics.frgimages.com/st-louis-cardinals/st-louis-cardinals-12-x-12-baseball-sign_pi3475000_ff_3475027-7c1fcf763a423f2c86fe_full.jpg?_hv=2&w=900';
+  image3 = 'https://www.iregistertrademarks.com/wp-content/uploads/large-1.png';
 
   constructor() { }
 
